2da5d4b9356d71fac1e8371a5ff84eefb43659b8
basi <ishibashi.tomoyuki@gmail.com>
2018-02-26 17:22:52 +0900

    add async sample

---
+++ sample/speech-async-gcs.js
@@ -0,0 +1,57 @@
+// Imports the Google Cloud client library
+const Speech = require('@google-cloud/speech');
+
+// Your Google Cloud Platform project ID
+const projectId = 'your-project-id'; // NOTE: 適切に変更する
+
+// Creates a client
+const client = new Speech.SpeechClient({
+  projectId: projectId,
+});
+
+// The Google Cloud Storage URI of the file on which to perform speech recognition, e.g. gs://my-bucket/audio.raw
+// const gcsUri = 'gs://basi-test/39110201742018011233050_1.flac';
+const gcsUri = 'gs://cloud-samples-tests/speech/brooklyn.flac';
+
+// The encoding of the audio file, e.g. 'LINEAR16'
+const encoding = 'FLAC';
+
+// The sample rate of the audio file in hertz, e.g. 16000
+const sampleRateHertz = 16000;
+
+// The BCP-47 language code to use, e.g. 'en-US'
+// const languageCode = 'ja-JP';
+const languageCode = 'en-US';
+
+const config = {
+  encoding: encoding,
+  sampleRateHertz: sampleRateHertz,
+  languageCode: languageCode,
+};
+
+const audio = {
+  uri: gcsUri
+};
+
+const request = {
+  config: config,
+  audio: audio
+};
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+client.longRunningRecognize(request)
+  .then((data) => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
+  .then((data) => {
+    const response = data[0];
+    response.results.forEach((result) => {
+      console.log(`Transcription: ${result.alternatives[0].transcript}`);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
---
+++ sample/speech-async-local.js
@@ -0,0 +1,53 @@
+// Imports the Google Cloud client library
+const Speech = require('@google-cloud/speech');
+const fs = require('fs');
+
+// Your Google Cloud Platform project ID
+const projectId = 'your-project-id'; // NOTE: 適切に変更する
+
+// Creates a client
+const client = new Speech.SpeechClient({
+  projectId: projectId,
+});
+
+// The name of the audio file to transcribe
+const fileName = './resources/filename.flac'; // NOTE: 適切に変更する
+
+// Reads a local audio file and converts it to base64
+const file = fs.readFileSync(fileName);
+const audioBytes = file.toString('base64');
+
+// The audio file's encoding, sample rate in hertz, and BCP-47 language code
+const audio = {
+  content: audioBytes,
+};
+
+// NOTE: 適切に変更する
+const config = {
+  encoding: 'FLAC',
+  sampleRateHertz: 48000,
+  languageCode: 'ja-JP',
+};
+
+const request = {
+  audio: audio,
+  config: config,
+};
+
+// Detects speech in the audio file. This creates a recognition job that you
+// can wait for now, or get its result later.
+client.longRunningRecognize(request)
+  .then((data) => {
+    const operation = data[0];
+    // Get a Promise representation of the final result of the job
+    return operation.promise();
+  })
+  .then((data) => {
+    const response = data[0];
+    response.results.forEach((result) => {
+      console.log(`Transcription: ${result.alternatives[0].transcript}`);
+    });
+  })
+  .catch((err) => {
+    console.error('ERROR:', err);
+  });
