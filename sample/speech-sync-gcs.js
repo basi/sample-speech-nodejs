45469323cda6a5c026be0fa0f7c3a8f0df54f3bb
basi <ishibashi.tomoyuki@gmail.com>
2018-02-26 18:40:30 +0900

    fix call method

--- sample/speech-sync-gcs.js
+++ sample/speech-sync-gcs.js
@@ -35,19 +35,13 @@ const request = {
   audio: audio
 };
 
-// Detects speech in the audio file. This creates a recognition job that you
-// can wait for now, or get its result later.
-client.longRunningRecognize(request)
-  .then((data) => {
-    const operation = data[0];
-    // Get a Promise representation of the final result of the job
-    return operation.promise();
-  })
+// Detects speech in the audio file
+client.recognize(request)
   .then((data) => {
     const response = data[0];
     const transcription = response.results.map(result =>
         result.alternatives[0].transcript).join('\n');
-    console.log(`Transcription: ${transcription}`);
+    console.log(`Transcription: `, transcription);
   })
   .catch((err) => {
     console.error('ERROR:', err);
